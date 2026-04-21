ecabea5493f8fd74d1161e7fa94788e168b61de6
Mattheus Adhonnay <mattheusadhonnaysf@hotmail.com>
2026-04-21 15:20:54 -0300

    feat: header component layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('go-task');
